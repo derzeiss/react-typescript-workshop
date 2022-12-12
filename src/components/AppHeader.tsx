7df1ce4b3212a5dfb81aa75ac4171b1bd5cfc3ba
alex <alex.zeiss@gmx.net>
2022-12-12 16:47:36 +0100

    Add Theme context

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { ThemeContext } from "../domain/theme/ThemeContext";
+
 export const AppHeader = () => {
+  const theme = useContext(ThemeContext);
+
   return (
     <header className="app-header">
-      <h1>Bookmonkey</h1>
+      <h1 style={{ color: theme.primaryColor }}>Bookmonkey</h1>
     </header>
   );
 };
