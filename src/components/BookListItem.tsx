2c8c42fd9f83d8b907663f016c1323e6c2476977
alex <alex.zeiss@gmx.net>
2022-12-12 16:20:14 +0100

    Display money bag next to free books

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -5,9 +5,14 @@ interface BookListItemProps {
 }
 
 export const BookListItem: React.FC<BookListItemProps> = ({ book }) => {
+  const isFree = book.price === "$0.00";
+
   return (
-    <div className="book-list-item">
-      <h2>{book.title}</h2>
+    <div className={`book-list-item ${isFree ? "book-list-item_free" : ""}`}>
+      <h2>
+        {isFree && <span>💰 </span>}
+        {book.title}
+      </h2>
       <h3>{book.subtitle}</h3>
       <div className="text-meta">by {book.author}</div>
     </div>
