c1b894794fcfef7c49d48a2a60e099c4c31d6d9d
alex <alex.zeiss@gmx.net>
2022-12-12 19:24:33 +0100

    Save changes

--- src/domain/book/api.ts
+++ src/domain/book/api.ts
@@ -11,3 +11,14 @@ export const fetchBook = (isbn: string) => {
   );
   return result as Promise<Book>;
 };
+
+export const updateBook = (book: Book) => {
+  const result = fetch(`http://localhost:4730/books/${book.isbn}`, {
+    method: "put",
+    body: JSON.stringify(book),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => (res.ok ? res.json() : Promise.reject(res)));
+  return result as Promise<Book>;
+};
--- src/screens/BookEditScreen.tsx
+++ src/screens/BookEditScreen.tsx
@@ -1,25 +1,30 @@
 import { FormEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchBook } from "../domain/book/api";
+import { fetchBook, updateBook } from "../domain/book/api";
+import { Book } from "../domain/book/Book";
 
 export const BookEditScreen = () => {
   const { isbn } = useParams<{ isbn: string }>();
+  const [book, setBook] = useState<Book>();
   const [title, setTitle] = useState("");
   const titleError =
     title.length < 5 && "The title must be at least 5 characters long.";
 
   useEffect(() => {
     if (!isbn) return;
-    fetchBook(isbn).then((book) => {
-      setTitle(book.title);
+    fetchBook(isbn).then((b) => {
+      setBook(b);
+      setTitle(b.title);
     });
   }, [isbn]);
 
   const handleSubmit = (ev: FormEvent) => {
     ev.preventDefault();
-    if (titleError) return;
+    if (titleError || !book) return;
 
-    alert(`entered title: "${title}"`);
+    updateBook({ ...book, title })
+      .then((data) => console.log("Updated successfully. Response:", data))
+      .catch((err) => console.error("Error while updating book. Error:", err));
   };
 
   return (
