b0a07b6b62339fd679f557451325930019129fe7
alex <alex.zeiss@gmx.net>
2022-12-12 16:26:41 +0100

    Add likes counter

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Book } from "../domain/book/Book";
 
 interface BookListItemProps {
@@ -5,16 +6,26 @@ interface BookListItemProps {
 }
 
 export const BookListItem: React.FC<BookListItemProps> = ({ book }) => {
+  const [likes, setLikes] = useState(0);
   const isFree = book.price === "$0.00";
+
+  const handleLikeClick = () => {
+    setLikes(likes + 1);
+  };
 
   return (
     <div className={`book-list-item ${isFree ? "book-list-item_free" : ""}`}>
       <h2>
+        {likes >= 5 && <span>⭐ </span>}
         {isFree && <span>💰 </span>}
         {book.title}
       </h2>
       <h3>{book.subtitle}</h3>
       <div className="text-meta">by {book.author}</div>
+      <button className="secondary" onClick={handleLikeClick}>
+        <span>👏</span>
+        {likes}
+      </button>
     </div>
   );
 };
