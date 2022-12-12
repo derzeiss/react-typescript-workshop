3e336ef3f0fddbfe8fc78845a1768603cb7860d3
alex <alex.zeiss@gmx.net>
2022-12-12 16:51:40 +0100

    Change default theme color

--- src/domain/theme/ThemeContext.ts
+++ src/domain/theme/ThemeContext.ts
@@ -2,6 +2,6 @@ import React from "react";
 import { Theme } from "./Theme";
 
 export const ThemeContext = React.createContext<Theme>({
-  primaryColor: "tomato",
+  primaryColor: "darkgrey",
   setPrimaryColor: () => {},
 });
