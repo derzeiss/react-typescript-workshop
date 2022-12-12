48ffb2fae7b4e93c90db392fb062079d07e0362d
alex <alex.zeiss@gmx.net>
2022-12-12 16:07:52 +0100

    Display example book

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { AppHeader } from "./components/AppHeader";
+import { ExampleBook } from "./components/ExampleBook";
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <ExampleBook />
     </div>
   );
 }
