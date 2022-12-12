bbbdd235e2481caf2087e4c90738c0e49cb9e304
alex <alex.zeiss@gmx.net>
2022-12-12 17:31:45 +0100

    Add nav links

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import { ThemeContext } from "../domain/theme/ThemeContext";
 import { ThemeEditor } from "./ThemeEditor";
 
@@ -9,6 +10,10 @@ export const AppHeader = () => {
     <header className="app-header">
       <ThemeEditor />
       <h1 style={{ color: theme.primaryColor }}>Bookmonkey</h1>
+      <nav>
+        <NavLink to="">Books</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
     </header>
   );
 };
